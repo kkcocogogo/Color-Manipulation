e13194843572f04ce411d57482713f0002e6e3ef
Tim Feirg <kkcocogogo@gmail.com>
2014-04-26 18:26:33 +0800

    clrman::pseudoColor is complete

--- clrman.cpp
+++ clrman.cpp
@@ -1,6 +1,8 @@
 #include "clrman.h"
 
-// this file includes all clrman(color manipulation) functions
+/*****************************************************
+ * clrman::cvt2websafe function
+*****************************************************/
 
 void clrman::cvt2websafe(cv::Mat& src, cv::Mat& dst) {
     assert(src.type()==16);
@@ -23,11 +25,21 @@ void clrman::cvt2websafe(cv::Mat& src, cv::Mat& dst) {
     }
 }
 
-// trying to design a function template that work with RGB images with arbitrary depth
-// if there's any way that I can use different data type other than double to hold the threthold!
-// to specify channel, use something like CLRMAN_BLUE
+/*****************************************************
+ * clrman::pseudoColor function
+ * the function work with RGB images with arbitrary depth
+ * there's no reason to use double for threthold values,
+ * but here the shortest data type appropriate depends on
+ * the color depth of the source image.
+ *
+ * to specify channel, use something like CLRMAN_BLUE
+*****************************************************/
+
 template<typename GRAY_TYPE, typename RGB_TYPE>
 void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst, double upperBound, double lowerBound , int channel) {
+    // calculate lootup table for each channel
+    // but what size should it take?
+    // forget about CLUT
     assert(
                 gray_src.channels() == 1 &&
                 rgb_dst.channels() == 3 &&
@@ -61,3 +73,49 @@ void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst, double upperBound,
     }
 }
 template void clrman::pseudoColor<uchar, cv::Vec3b>(cv::Mat &gray_src, cv::Mat &rgb_dst, double upperBound, double lowerBound , int channel);
+
+/*****************************************************
+ * this is the standard pseudo color conversion
+ * please refer to this website:
+ * http://blog.damiles.com/2010/01/pseudocolor-implementation-with-opencv/
+ * this is where I get the constants for the following program
+ * the output of this function is fixed to cv::Vec3d
+*****************************************************/
+void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst) {
+
+    // create a normalized copy of source image, this new source'll be used throughout the function
+    cv::Mat double_src;
+    gray_src.convertTo(double_src, CV_64F);
+    // normalize double_src so that sine calculations will work
+    cv::normalize(double_src, double_src, M_PI, 0, cv::NORM_MINMAX);
+    // create dst as 64F
+    rgb_dst = cv::Mat_<cv::Vec3d>(gray_src.size());
+    // create iterators
+    cv::MatIterator_<double> it_src = double_src.begin<double>();
+    cv::MatIterator_<cv::Vec3d> it_dst = rgb_dst.begin<cv::Vec3d>();
+    cv::MatIterator_<double> it_end = double_src.end<double>();
+    // obtain a copy of the brightest value within source image
+    //    const double
+    //            src_max = *(std::max_element(it_src, it_end)),
+    //            src_min = *(std::min_element(it_src, it_end));
+
+    for(; it_src != it_end; it_src ++, it_dst ++) {
+        double pixGrayLevel_src = *it_src;
+
+        (*it_dst)[0] = fabs(sin( pixGrayLevel_src * 2));
+        (*it_dst)[1] = fabs(sin( pixGrayLevel_src * 2 + (-0.1) * M_PI));
+        (*it_dst)[2] = fabs(sin( pixGrayLevel_src * 2 + (-0.3) * M_PI));
+
+//        {
+//            using namespace std;
+//            using namespace cv;
+//            imshow("current",rgb_dst);
+//            waitKey(1);
+//            cout<<*it_dst<<endl;
+//            cv::waitKey();
+//        }
+
+    }
+    // normalize dst so that it can be perceived by eye
+//    cv::normalize(rgb_dst, rgb_dst, 1, 0, cv::NORM_MINMAX);
+}
--- clrman.h
+++ clrman.h
@@ -3,6 +3,8 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <iostream>
 
 #define CLRMAN_BLUE 0
 #define CLRMAN_GREEN 1
@@ -17,5 +19,6 @@ void cvt2websafe(cv::Mat& src, cv::Mat& dst);
 template<typename GRAY_TYPE, typename RGB_TYPE>
 void pseudoColor(cv::Mat &src, cv::Mat &dst, double upperBound, double lowerBound, int channel );
 
+void pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst);
 }
 #endif // CLRMAN_H
