ca76388cf716b224ddb62d5356862130b2484e42
Tim Feirg <kkcocogogo@gmail.com>
2014-04-27 16:36:25 +0800

    refined for demo
    add everything in main.cpp for demo

--- clrman.cpp
+++ clrman.cpp
@@ -2,31 +2,104 @@
 
 /*****************************************************
  * clrman::cvt2websafe function
+ * this function works with gray or rgb images with
+ * depth of 8/16
 *****************************************************/
-
-void clrman::cvt2websafe(cv::Mat& src, cv::Mat& dst) {
-    assert(src.type()==16);
-    // preallocate size to speed things up, not quite sure of its merits
-    if (dst.size() != src.size()) {
-        dst = cv::Mat_<cv::Vec3b>(src.size());
+void clrman::cvt2websafe(cv::Mat &src, cv::Mat &dst) {
+    int channels = src.channels();
+    assert(channels == 1 || channels == 3);
+    // force convert to depth of 8, initialize the size of dst
+    if (src.depth() > sizeof(uchar) || dst.size() != src.size()) {
+        if(channels == 3) {
+            src.convertTo(src, CV_8UC3);
+            dst = cv::Mat(src.size(), CV_8UC3);
+        }
+        else {
+            src.convertTo(src, CV_8UC1);
+            dst = cv::Mat(src.size(), CV_8UC1);
+        }
     }
-    cv::MatIterator_<cv::Vec3b>
-            it_src = src.begin<cv::Vec3b>(),
-            it_dst = dst.begin<cv::Vec3b>(),
-            it_end = src.end<cv::Vec3b>();
 
-    // round all value to the nearest multiple of 51 to maintain accuracy
-    // better ways to do this is using a lookup table, but I'm somehow stuck at generating such array
-    // DONT use Mat::at in this case, MatIterator are for scanning images ad-hoc
-    for( ; it_src != it_end; it_src ++, it_dst ++) {
-        (*it_dst)[0] = (((*it_src)[0] + 26) / 51) * 51;
-        (*it_dst)[1] = (((*it_src)[1] + 26) / 51) * 51;
-        (*it_dst)[2] = (((*it_src)[2] + 26) / 51) * 51;
+    // this will bring more efficiency, go refer to http://docs.opencv.org/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html
+    int rows = src.rows * channels;
+    int cols = src.cols;
+    if(src.isContinuous()) {
+        cols *= rows;
+        rows = 1;
+    }
+
+    int i,j;
+    uchar* p_src;
+    uchar* p_dst;
+    for(i = 0; i < rows; ++i) {
+        p_src = src.ptr<uchar>(i);
+        p_dst = dst.ptr<uchar>(i);
+        for(j = 0; j < cols; ++j) {
+            p_dst[j] = ((p_src[j] + 26) / 51) * 51;
+        }
     }
 }
 
 /*****************************************************
- * THIS FUNCTION IS CONSIDERED USELESS
+ * this is the standard pseudo color conversion
+ * please refer to this website:
+ * http://blog.damiles.com/2010/01/pseudocolor-implementation-with-opencv/
+ * this is where I get the constants for the following program
+ * the output of this function is fixed to cv::Vec3d
+*****************************************************/
+void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst) {
+
+    // create a normalized copy of source image, this new source'll be used throughout the function
+    cv::Mat double_src;
+    gray_src.convertTo(double_src, CV_64F);
+    // normalize double_src so that sine calculations will work
+    cv::normalize(double_src, double_src, M_PI, 0, cv::NORM_MINMAX);
+    // create dst as 64F
+    rgb_dst = cv::Mat_<cv::Vec3d>(gray_src.size());
+    // create iterators
+    cv::MatIterator_<double> it_src = double_src.begin<double>();
+    cv::MatIterator_<cv::Vec3d> it_dst = rgb_dst.begin<cv::Vec3d>();
+    cv::MatIterator_<double> it_end = double_src.end<double>();
+
+    for(; it_src != it_end; it_src ++, it_dst ++) {
+        double pixGrayLevel_src = *it_src;
+
+        (*it_dst)[0] = fabs(sin( pixGrayLevel_src * 2));
+        (*it_dst)[1] = fabs(sin( pixGrayLevel_src * 2 + (-0.1) * M_PI));
+        (*it_dst)[2] = fabs(sin( pixGrayLevel_src * 2 + (-0.3) * M_PI));
+    }
+}
+
+/*****************************************************
+ * this function implements histogram equalization on all
+ * three channels separately
+ * as a bonus it also takes gray scale image
+*****************************************************/
+void clrman::histoEqual(cv::Mat &src, cv::Mat &dst) {
+    int channels = src.channels();
+    // crash the program if images with weird formats comes in
+    assert(channels == 1 || channels == 3);
+
+    // for gray scale images
+    if(channels == 1) cv::equalizeHist(src, dst);
+    // for rgb images
+    else if(channels == 3){
+        cv::Mat channelContainer[3];
+        cv::split(src,channelContainer);
+        // equalize all 3 channels
+        cv::equalizeHist(channelContainer[0],channelContainer[0]);
+        cv::equalizeHist(channelContainer[1],channelContainer[1]);
+        cv::equalizeHist(channelContainer[2],channelContainer[2]);
+        // merge into one rgb mat
+        cv::merge(channelContainer,3,dst);
+    }
+}
+
+/*****************************************************
+ * I tried (and failed) to design a function that convert
+ * images with arbitary depth to pseudo color with user-
+ * specified thretholds.
+ * right now THIS FUNCTION IS CONSIDERED USELESS
  * clrman::pseudoColor function
  * the function work with RGB images with arbitrary depth
  * there's no reason to use double for threthold values,
@@ -74,53 +147,3 @@ void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst, double upperBound,
     }
 }
 template void clrman::pseudoColor<uchar, cv::Vec3b>(cv::Mat &gray_src, cv::Mat &rgb_dst, double upperBound, double lowerBound , int channel);
-
-/*****************************************************
- * this is the standard pseudo color conversion
- * please refer to this website:
- * http://blog.damiles.com/2010/01/pseudocolor-implementation-with-opencv/
- * this is where I get the constants for the following program
- * the output of this function is fixed to cv::Vec3d
-*****************************************************/
-void clrman::pseudoColor(cv::Mat &gray_src, cv::Mat &rgb_dst) {
-
-    // create a normalized copy of source image, this new source'll be used throughout the function
-    cv::Mat double_src;
-    gray_src.convertTo(double_src, CV_64F);
-    // normalize double_src so that sine calculations will work
-    cv::normalize(double_src, double_src, M_PI, 0, cv::NORM_MINMAX);
-    // create dst as 64F
-    rgb_dst = cv::Mat_<cv::Vec3d>(gray_src.size());
-    // create iterators
-    cv::MatIterator_<double> it_src = double_src.begin<double>();
-    cv::MatIterator_<cv::Vec3d> it_dst = rgb_dst.begin<cv::Vec3d>();
-    cv::MatIterator_<double> it_end = double_src.end<double>();
-
-    for(; it_src != it_end; it_src ++, it_dst ++) {
-        double pixGrayLevel_src = *it_src;
-
-        (*it_dst)[0] = fabs(sin( pixGrayLevel_src * 2));
-        (*it_dst)[1] = fabs(sin( pixGrayLevel_src * 2 + (-0.1) * M_PI));
-        (*it_dst)[2] = fabs(sin( pixGrayLevel_src * 2 + (-0.3) * M_PI));
-    }
-}
-
-/*****************************************************
- * this function implements histogram equalization on all
- * three channels separately
-*****************************************************/
-void clrman::histoEqual(cv::Mat &src, cv::Mat &dst) {
-    int channels = src.channels();
-    assert(channels == 1 || channels == 3);
-    if(channels == 1) cv::equalizeHist(src, dst);
-    else if(channels == 3){
-        cv::Mat channelContainer[3];
-        cv::split(src,channelContainer);
-        // equalize all 3 channels
-        cv::equalizeHist(channelContainer[0],channelContainer[0]);
-        cv::equalizeHist(channelContainer[1],channelContainer[1]);
-        cv::equalizeHist(channelContainer[2],channelContainer[2]);
-        // merge into one rgb mat
-        cv::merge(channelContainer,3,dst);
-    }
-}
--- main.cpp
+++ main.cpp
@@ -4,34 +4,44 @@
 
 int main(int argc, char *argv[])
 {
-//        QApplication colorManipulation(argc,argv);
-//        MainWindow mainwindow;
-//        mainwindow.show();
-//        return colorManipulation.exec();
+    {
+        using namespace cv;
+        Mat gradBW = cv::imread("/Users/timfeirg/Documents/Color-Manipulation/gradBW.tif",0);
+        Mat rgbcube = cv::imread("/Users/timfeirg/Documents/Color-Manipulation/WebSafeTarget.png");
+        Mat rgbimage = cv::imread("/Users/timfeirg/Documents/Color-Manipulation/rgbImage.tif");
+        Mat washinton = imread("/Users/timfeirg/Documents/Color-Manipulation/washinton.tif");
+        Mat dst;
+        // pseudo color problem
+        clrman::pseudoColor(gradBW,dst);
+        imshow("gradient",gradBW);
+        imshow("pseudo color map",dst);
+        waitKey();
+        clrman::pseudoColor(washinton,dst);
+        imshow("real case BW image",dst);
+        imshow("washinton to be convert to pseudo color",washinton);
+        waitKey();
+        // web safe color
+        clrman::cvt2websafe(rgbcube,dst);
+        imshow("HSI cube",rgbcube);
+        imshow("web safe color",dst);
+        waitKey();
+        // histogram equalization
+        clrman::histoEqual(rgbimage,dst);
+        imshow("after histo equalization",dst);
+        imshow("read case rgb image",rgbimage);
+        waitKey();
+    }
 
-
-
-    cv::Mat src = cv::imread("/Users/timfeirg/Google Drive/OpenCV/DIP3E_Original_Images_CH06/Fig0627(a)(WashingtonDC Band3-RED).TIF",0);
-    cv::Mat rgb_src = cv::imread("/Users/timfeirg/Google Drive/OpenCV/DIP3E_Original_Images_CH06/Fig0608(RGB-full-color-cube).tif");
-    cv::Mat rgb_635 = cv::imread("/Users/timfeirg/Google Drive/OpenCV/DIP3E_Original_Images_CH06/Fig0635(middle_row_left_chalk ).tif");
-    cv::Mat dst;
-
-    cv::Mat grad = cv::imread("/Users/timfeirg/Documents/Color-Manipulation/gradBW.tif",0);
-    dst.create(src.size(),CV_8UC3);
-
-//        clrman::cvt2websafe(rgb_src,dst);
-    double upperbound = 90.0;
-    double lowerbould = 60.0;
-    int channel = CLRMAN_RED;
-    clrman::histoEqual(rgb_635,dst);
-//    clrman::pseudoColor(grad,dst);
-//    clrman::pseudoColor<uchar, cv::Vec3b>(src, dst, upperbound, lowerbould, channel);
-//    cv::imshow("src", grad);
-    cv::imshow("rgb 635", rgb_635);
-//    cv::imshow("src",src);
-    cv::imshow("dst",dst);
-    cv::waitKey();
     return 1;
+    /****************************************
+     * this is the code of my unfinished
+     * project, I'm keeping it as a debugging
+     * area, have fun with everything above
+    ****************************************/
+    //        QApplication colorManipulation(argc,argv);
+    //        MainWindow mainwindow;
+    //        mainwindow.show();
+    //        return colorManipulation.exec();
 }
 
 
